6005b58674765025906d3cd80ac49257c1eb64aa
robertzhangwenjie <robertzhangwenjie@gmail.com>
2022-09-22 10:20:44 +0800

    test(git): remove testcase of git.InstallSubCommand
    reason: InstallSubCmd depends on the Git installation directory which is indeterminate, and its basically calling functions copyFile implement install, so it should be tested at a high level such as integration testing

--- git/install_test.go
+++ git/install_test.go
@@ -1,7 +1,7 @@
 /*
  * @Author: robert zhang <robertzhangwenjie@gmail.com>
  * @Date: 2022-08-06 16:52:22
- * @LastEditTime: 2022-09-22 00:34:02
+ * @LastEditTime: 2022-09-22 10:11:03
  * @LastEditors: robert zhang
  * @Description:
  */
@@ -16,33 +16,6 @@ import (
 
 	. "github.com/smartystreets/goconvey/convey"
 )
-
-func TestInstallSubCmd(t *testing.T) {
-	Convey("Given a file which doesn't exist", t, func() {
-		path := "/123/456"
-		subCmdName := "123"
-		Convey("When install it as git subcommand", func() {
-			_, err := InstallSubCmd(path, subCmdName)
-			Convey("Then should return err", func() {
-				So(err, ShouldNotBeNil)
-			})
-		})
-	})
-
-	Convey("Given a exists cmd file", t, func() {
-		path := filepath.Join("testdata", "testcmd")
-		subCmdName := "testcmd"
-		Convey("When install it as git subcommand", func() {
-			cmdPath, err := InstallSubCmd(path, subCmdName)
-			if err == nil {
-				defer os.Remove(cmdPath)
-			}
-			Convey("Then it should return nil", func() {
-				So(err, ShouldBeNil)
-			})
-		})
-	})
-}
 
 func Test_copyFile(t *testing.T) {
 	Convey("Given a temporary dir", t, func() {
